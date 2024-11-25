e62c1b028224983e12fa0ca7847f4b2c5feeb002
Ben Krusekamp <krusekamp@fzi.de>
2024-11-25 18:03:45 +0100

    add xbar main

--- src/system.sv
+++ src/system.sv
@@ -9,19 +9,34 @@ module system #(
     input logic uart_tx_o
 ); 
 
-// management core data and instr
+// --- tlul declaration ---
 tlul_pkg::tl_h2d_t management_core_instr_req;
 tlul_pkg::tl_d2h_t management_core_instr_rsp;
 tlul_pkg::tl_h2d_t management_core_data_req;
 tlul_pkg::tl_d2h_t management_core_data_rsp;
 
-
-// management core sram
 tlul_pkg::tl_h2d_t management_scratchpad_instr_req;
 tlul_pkg::tl_d2h_t management_scratchpad_instr_rsp;
 tlul_pkg::tl_h2d_t management_scratchpad_data_req;
 tlul_pkg::tl_d2h_t management_scratchpad_data_rsp;
 
-// management core uart
 tlul_pkg::tl_h2d_t uart_req;
 tlul_pkg::tl_d2h_t uart_rsp;
+
+xbar_main #() u_xbar_main (
+    .clk_main_i(clk_sys_i),
+    .rst_main_ni(srt_sys_ni),
+
+    .tl_management_core_instr_req(management_core_instr_req),
+    .tl_management_core_instr_rsp(management_core_instr_rsp),
+    .tl_management_core_data_req(management_core_data_req),
+    .tl_management_core_data_rsp(management_core_data_rsp),
+
+    .tl_management_scratchpad_instr_req(management_scratchpad_instr_req),
+    .tl_management_scratchpad_instr_rsp(management_scratchpad_instr_rsp),
+    .tl_management_scratchpad_data_req(management_scratchpad_data_req),
+    .tl_management_scratchpad_data_rsp(management_scratchpad_data_rsp),
+
+    .tl_uart_req(uart_req),
+    .tl_uart_rsp(uart_rsp),
+);
